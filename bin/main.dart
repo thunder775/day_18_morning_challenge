4f69137b9343206730acfe1b00263a40947530b3
McLarenCollege <hello@mclarencollege.in>
2019-10-24 08:31:05 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,43 @@
+// Challenge 1
+// Write test cases for next 2 challenges. Also include tests for boundary conditions
+// For this challenge, the input will be a (long) string.
+
+
+// Challenge 2
+// A word encountered for the first time is a stranger.
+// A word encountered thrice becomes an acquaintance.
+// A word encountered 5 times becomes a friend.
+
+// Create a function that takes the string and returns a list of two lists.
+// The first is a list of acquaintances in the order they became an acquaintance (see example).
+// The second is a list of friends in the order they became a friend.
+// Words in the friend list should no longer be in the acquaintance list.
+
+// Example
+// noStrangers("See Spot run. See Spot jump. Spot likes jumping. See Spot fly.")
+// ➞ [["spot", "see"], []]
+/// "see" was encountered first but "spot" became an acquaintance earlier.
+
+
+
+// Challenge 3
+// Rhyme Time
+// Create a function that returns true if two lines rhyme and false otherwise.
+// For the purposes of this exercise, two lines rhyme if the last
+// word from each sentence contains the same vowels.
+// Example:
+// doesRhyme("Sam I am!", "Green eggs and ham.") ➞ true
+
+
+// Challenge 4
+// Do All Bigrams (2 character words) Exist?
+// You are given an input list of bigrams, and a list of words.
+//
+// Write a function that returns true if you can find every single bigram from
+// this list can be found at least once in an list of words.
+//
+// Examples
+// canFind(["at", "be", "th", "au"], ["beautiful", "the", "hat"]) ➞ true
+main() {
+
+}
